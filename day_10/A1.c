0bc59d279e7066be2a7919118050ad22730434ba
Poojitha-09 <nagapoojitha1234@gmail.com>
2024-11-08 00:00:18 +0530

    Create A1.c

---
+++ day_10/A1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int isPrime(int n);
+int main()
+{  
+    int i ,n; 
+    int count=0;
+    printf("enter the positive number : "); 
+    scanf("%d", &n); 
+    for( i= 2; i <= n/2; i++) 
+    {
+        if(isPrime(i) && isPrime(n-i))
+        {
+            printf("\n%d = %d + %d\n", n, i ,n-i); 
+            count ++;
+        }
+    } 
+    if(count == 0)
+        printf("\n NoofWays = %d",-1);  
+    else
+        printf("\n NoofWays = %d",count); 
+    
+    return 0;
+}
+int isPrime(int n) 
+{
+    int i;
+    if(n<=1)
+        return 0;
+    for(i=2; i * i<= n;i++) 
+    {
+        if( n%i == 0)
+            return 0;
+    }
+    return 1;
+}
